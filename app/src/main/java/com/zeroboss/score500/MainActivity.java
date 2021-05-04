6384315ca055088dc5920d5194bdda85a8a25cc8
brucieW <bruciew50@gmail.com>
2021-05-04 20:59:50 +1000

    First commit.

---
+++ app/src/main/java/com/zeroboss/score500/MainActivity.java
@@ -0,0 +1,28 @@
+package com.zeroboss.score500;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button button;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(view -> Toast.makeText(this, "Hello " + editText.getText().toString(), Toast.LENGTH_LONG).show());
+    }
+}
